e570f4337b48ea63864899b0940a22c474a41195
Youness Houdass <youness.h@hiitconsulting.com>
2019-02-05 10:07:32 +0000

    Devtools Branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoInfoComponent } from './todo/todo-info/todo-info.component';
-import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
 import { todoReducer } from './todo/todo.reducer';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoInfoComponent],
-  imports: [BrowserModule, FormsModule, StoreModule.forRoot({ todo: todoReducer })],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ todo: todoReducer }),
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 10 }) : []
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
